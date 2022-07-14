e6a30fc02b3848a3827fd3f13e7665ccfa08c869
Dinesh Panchal <dineshpanchal432@gmail.com>
2022-07-14 16:21:43 +0530

    Update events.py

--- tuna_ird/events.py
+++ tuna_ird/events.py
@@ -1,7 +1,7 @@
 import frappe
 
 def update_sales_invoice_oncancel(doc,method =None):
-    frappe.db.set_value("Sales Invoice",doc.name,{"title":"Cancelled","customer_name":"Cancelled","customer":"Cancelled","total_qty":0,"total":0,"grand_total":0,"rounded_total":0,"in_words":"NA","rounding_adjustment":0,"total_taxes_and_charges":0,"outstanding_amount":0,"base_net_total":0})
+    frappe.db.set_value("Sales Invoice",doc.name,{"title":"Cancelled","customer_name":"Cancelled","customer":"Cancelled","total_qty":0,"total":0,"grand_total":0,"rounded_total":0,"in_words":"NA","rounding_adjustment":0,"total_taxes_and_charges":0,"outstanding_amount":0,"base_net_total":0,"total_tax":0,"total_tax":0,"base_rounded_total":0,"outstanding_amount":0})
     frappe.db.commit()
     for i in doc.items:
         db_update = frappe.db.set_value("Sales Invoice Item",i.name,{"qty":0,"rate":0,"amount":0,"base_rate":0,"base_amount":0,
